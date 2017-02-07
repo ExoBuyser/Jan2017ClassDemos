39c97d1c8fa27b20d6bfcaf9c24f4324a7735184
ExoBuyser <ebuyser1@studentmail.nait.ca>
2017-02-06 19:11:31 -0700

    some validation changes

--- ClassDemo/Chinook.Data/Entities/Track.cs
+++ ClassDemo/Chinook.Data/Entities/Track.cs
@@ -17,33 +17,31 @@ namespace Chinook.Data.Entities
         [Key]
         public int TrackId { get; set; }
 
-        [Required(ErrorMessage = "Name is a Required field.")]
-        [StringLength(200, ErrorMessage="Name is limited to 200 characters")]
+        [Required(ErrorMessage = "Name is a required field.")]
+        [StringLength(200, ErrorMessage = "Name is limited to 200 characters.")]
         public string Name { get; set; }
-
-
-        public int AlbumId { get; set; }
+        public int? AlbumId { get; set; }
         public int MediaTypeId { get; set; }
-        public int GenreId { get; set; }
+        public int? GenreId { get; set; }
 
-        [StringLength(220, ErrorMessage = "Composer is limited to 220 characters")]
+        [StringLength(220, ErrorMessage = "Composer is limited to 220 characters.")]
         public string Composer { get; set; }
 
-        [Required(ErrorMessage="Milliseconds is a required field")]
-        [Range(1.0,double.MaxValue,ErrorMessage="Milliseconds value out of range; must be greater than 0")]
-        public int Miliseconds { get; set; }
-
+        [Required(ErrorMessage = "Milliseconds is a required filed")]
+        [Range(1.0, double.MaxValue, ErrorMessage = "Millisecond value out of range; must be greater that 0.")]
+        public int Milliseconds { get; set; }
         public int? Bytes { get; set; }
-
-
-
+        [Required(ErrorMessage = "Unit Price is a required filed")]
+        [Range(0.00, double.MaxValue, ErrorMessage = "Unit Price value out of range; must be 0 or greater.")]
         public decimal UnitPrice { get; set; }
 
+        //Navigation properties
         public virtual Album Album { get; set; }
         public virtual MediaType MediaType { get; set; }
 
-        //public virtual Genre Genre {get;set;}
-        //public virtual ICollection<PlaylistTrack> PlaylisTracks {get;set;}
+        //public virtual Genre Genre{get;set;}
+        //public virtual ICollection<PlaylistTrack> PlaylistTracks {get;set;}
         //public virtual ICollection<InvoiceLine> InvoiceLines{get;set;}
+
     }
 }
